2c766c08d33b2d9fb6387e19e3150d9e9564a9b8
Will Richards <richardsw@carleton.edu>
2016-07-01 22:26:04 -0600

    searches all NASDAQ stocks

--- stocks.py
+++ stocks.py
@@ -1,14 +1,25 @@
+###
+###  Python Web Scraper for stock prices
+###  Created by Will Richards
+###
 import urllib.request
 import re
 
-htmlfile = urllib.request.urlopen("http://finance.yahoo.com/q?s=aapl&fr=uh3_finance_web&uhb=uhb2")
+stock_file = open("stocklist.txt")
 
-htmltext = htmlfile.read()
+stock_list2 = stock_file.read()
 
-regex = "<span id=\"yfs_l84_aapl\">(.+?)</span>"
+stock_list = stock_list2.split("\n")
 
-pattern = re.compile(regex)
 
-price = re.findall(pattern,htmltext.decode("latin1"))
+i=0
 
-print (price)
+while i<len(stock_list):
+    url = "http://finance.yahoo.com/q?s=" + stock_list[i] + "&fr=uh3_finance_web&uhb=uhb2"
+    html_file = urllib.request.urlopen(url)
+    html_text = html_file.read()
+    regex = '<span id=\"yfs_l84_[^.]*\">(.+?)</span>'
+    pattern = re.compile(regex)
+    price = re.findall(pattern,html_text.decode("latin1"))
+    print ("the price of",stock_list[i], "is", price   )
+    i+=1
